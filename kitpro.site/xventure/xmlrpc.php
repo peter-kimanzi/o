9398d3ec3b1d646b5922b3e12fe169e66b40d1ef
Peter Kimanzi <71552773+peter-kimanzi@users.noreply.github.com>
2023-12-05 19:31:38 +0300

    first commit

---
+++ kitpro.site/xventure/xmlrpc.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://kitpro.site/xventure</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://kitpro.site/xventure/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://kitpro.site/xventure/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://kitpro.site/xventure/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://kitpro.site/xventure/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://kitpro.site/xventure/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
